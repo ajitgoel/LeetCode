a596a7fa2cb6cf6298af2676671db93092e9a299
Ajit Goel <ajitgoel@gmail.com>
2020-03-09 22:16:17 -0500

    rotten oranges

--- XUnitTestProject1/RottenOranges.cs
+++ XUnitTestProject1/RottenOranges.cs
@@ -13,83 +13,115 @@ namespace XUnitTestProject1
       ROTTEN = 2,
       FRESH = 1,
       EMPTY = 0
-  }
-    
+    }    
 
     [Fact]
     public void Test1()
     {
-      var grid = new int[3, 3] {{ 2, 1, 1},{ 1, 1, 0},{ 0, 1, 1} };
+      int[][] grid =  { new int[] { 2, 1, 1}, new int[] { 1, 1, 0}, new int[] { 0, 1, 1} };
       Assert.Equal(4, TimeToInfectAllOranges(grid));
     }
     [Fact]
     public void Test2()
     {
-      var grid = new int[3, 3] { {2, 1, 1},{ 0, 1, 1 },{ 1, 0, 1} };
+      int[][] grid = 
+        {
+          new int[] {2, 1, 1}, 
+          new int[] { 0, 1, 1 }, 
+          new int[] { 1, 0, 1} 
+        };
       Assert.Equal(-1, TimeToInfectAllOranges(grid));
     }
     [Fact]
     public void Test3()
     {
-      var grid = new int[1, 2] { { 0, 2 }};
+      int[][] grid = { new int[]{ 0, 2 }};
       Assert.Equal(0, TimeToInfectAllOranges(grid));
     }
 
-    public int TimeToInfectAllOranges(int[,] grid)
+    public int TimeToInfectAllOranges(int[][] grid)
     {
-      var rows = grid.GetLength(0);
-      var columns = grid.GetLength(1);
-      int timeElapsed = 0;
-      if (rows == 0 || rows > 10 || columns == 0 || columns > 10)
+      if (grid == null)
+      {
+        throw new ArgumentNullException(nameof(grid), "Grid input paramter should not be null");
+      }
+
+      var rows = grid.Length;
+      if (rows == 0 || rows > 10)
+      {
+        return 0;
+      }
+      //if no elements are fresh then return zero
+      if (grid.SelectMany(x=>x).ToArray().Any(x => x == (int)Element.FRESH) == false)
       {
         return 0;
       }
 
-      var validElements = new int[] { (int) Element.ROTTEN, (int)Element.FRESH, (int)Element.EMPTY }.Cast<int>();
-      if (grid.Cast<int>().Any(x => x == (int)Element.FRESH)==false)
+      var rottenOrangesLocation = new Queue<int[]>();
+      for (int row = 0; row < rows; row++)
       {
-        return 0;
-      }
-
-      while (true)
-      {
-        for (int row = 0; row < rows; row++)
+        var columns=grid[row].Length;
+        for (int column = 0; column < columns; column++)
         {
-          for (int column = 0; column < columns; column++)
+          var item = grid[row][column];
+          if (columns == 0 || columns > 10)
           {
-            var item = grid[row, column];
-            if (item == (int)Element.ROTTEN)
-            {
-              //itemLeft
-              if (column - 1 >= 0 && grid[row, column - 1] == (int)Element.FRESH)
-              {
-                grid[row, column - 1] = (int)Element.ROTTEN;
-              }
-              //itemRight
-              if (column + 1 < columns && grid[row, column + 1] == (int)Element.FRESH)
-              {
-                grid[row, column + 1] = (int)Element.ROTTEN;
-              }
-              //itemAbove
-              if (row - 1 >= 0 && grid[row - 1, column] == (int)Element.FRESH)
-              {
-                grid[row - 1, column] = (int)Element.ROTTEN;
-              }
-              //itemBelow
-              if (row + 1 < rows && grid[row + 1, column] == (int)Element.FRESH)
-              {
-                grid[row + 1, column] = (int)Element.ROTTEN;
-              }
-            }
+            return 0;
+          }
+
+          if (item == (int)Element.ROTTEN)
+          {
+            rottenOrangesLocation.Enqueue(new int[] { row, column });
+          }
+        }
+      }
+
+      int timeElapsed = 0;
+      while (rottenOrangesLocation.Count>0)
+      {
+        var rottenOrangesCount=rottenOrangesLocation.Count;
+        for (int counter = 0; counter < rottenOrangesCount; counter++)
+        {
+          var item = rottenOrangesLocation.Dequeue();
+          var row = item[0];
+          var column = item[1];
+          var columns = grid[row].Length;
+          //itemLeft
+          if (column - 1 >= 0 && grid[row][column - 1] == (int)Element.FRESH)
+          {
+            grid[row][column - 1] = (int)Element.ROTTEN;
+            rottenOrangesLocation.Enqueue(new int[] { row, column - 1 });
+          }
+          //itemRight
+          if (column + 1 < columns && grid[row][column + 1] == (int)Element.FRESH)
+          {
+            grid[row][column + 1] = (int)Element.ROTTEN;
+            rottenOrangesLocation.Enqueue(new int[] { row, column + 1 });
+          }
+          //itemAbove
+          if (row - 1 >= 0 && grid[row - 1][column] == (int)Element.FRESH)
+          {
+            grid[row - 1][column] = (int)Element.ROTTEN;
+            rottenOrangesLocation.Enqueue(new int[] { row - 1, column });
+          }
+          //itemBelow
+          if (row + 1 < rows && grid[row + 1][column] == (int)Element.FRESH)
+          {
+            grid[row + 1][column] = (int)Element.ROTTEN;
+            rottenOrangesLocation.Enqueue(new int[] { row + 1, column });
           }
         }
         timeElapsed += 1;
 
-        System.Diagnostics.Debug.WriteLine("grid: " + JsonConvert.SerializeObject(grid));
-        if (grid.Cast<int>().All(x => new List<int>() { (int)Element.ROTTEN, (int)Element.EMPTY }.Contains(x)) == true)
+        if (grid.SelectMany(x => x).ToArray().All(x => new[] { (int)Element.ROTTEN, (int)Element.EMPTY }.Contains(x)) == true)
         {
           break;
         }
+      }
+      //if any of the elements are still fresh then it means that they are unreachable, return -1 in this case
+      if (grid.SelectMany(x => x).ToArray().Any(x => x==(int)Element.FRESH) == true)
+      {
+        return -1;
       }
       return timeElapsed;
     }
