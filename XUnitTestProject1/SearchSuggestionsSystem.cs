75f6652f4bf0cf421b6fa71152cb90b255184588
Ajit Goel <ajitgoel@gmail.com>
2020-03-14 10:59:14 -0500

    SearchSuggestionsSystem

--- XUnitTestProject1/SearchSuggestionsSystem.cs
+++ XUnitTestProject1/SearchSuggestionsSystem.cs
@@ -1,4 +1,3 @@
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +7,6 @@ namespace XUnitTestProject1
 {  
   public class SearchSuggestionsSystem
   {
-    enum Element
-    {
-      WATER = '0',
-      LAND= '1'
-    }
-
     [Fact]
     public void Test1()
     {
@@ -25,7 +18,11 @@ namespace XUnitTestProject1
         new List<string> { "mouse", "mousepad" },
         new List<string> { "mouse", "mousepad" }
       };
-      Assert.Equal(output, SuggestedProducts(new[] { "mobile", "mouse", "moneypot", "monitor", "mousepad" }, "mouse"));
+      var result = SuggestedProducts(new[] { "mobile", "mouse", "moneypot", "monitor", "mousepad" }, "mouse");
+      for (int counter= 0;counter< output.Count; counter++)
+      {
+        Assert.True(output[counter].SequenceEqual(result[counter]));
+      }      
     }
 
     [Fact]
@@ -40,7 +37,12 @@ namespace XUnitTestProject1
         new List<string> { "havana"},
         new List<string> { "havana"}
       };
-      Assert.Equal(output, SuggestedProducts(new[] {"havana"}, "havana"));
+
+      var result = SuggestedProducts(new[] { "havana" }, "havana");
+      for (int counter = 0; counter < output.Count; counter++)
+      {
+        Assert.True(output[counter].SequenceEqual(result[counter]));
+      }
     }
     [Fact]
     public void Test3()
@@ -51,8 +53,12 @@ namespace XUnitTestProject1
         new List<string> { "baggage", "bags", "banner"},
         new List<string> { "baggage", "bags"},
         new List<string> { "bags"}
-      };
-      Assert.Equal(output, SuggestedProducts(new[] { "bags", "baggage", "banner", "box", "cloths" }, "bags"));
+      };      
+      var result = SuggestedProducts(new[] { "bags", "baggage", "banner", "box", "cloths" }, "bags");
+      for (int counter = 0; counter < output.Count; counter++)
+      {
+        Assert.True(output[counter].SequenceEqual(result[counter]));
+      }
     }
 
     [Fact]
@@ -68,97 +74,40 @@ namespace XUnitTestProject1
         new List<string> { "" },
         new List<string> { "" }
       };
-      Assert.Equal(output, SuggestedProducts(new[] { "havana" }, "tatiana"));
+      var result=SuggestedProducts(new[] { "havana" }, "tatiana");
+      for (int counter = 0; counter < output.Count; counter++)
+      {
+        Assert.True(output[counter].SequenceEqual(result[counter]));
+      }
     }
-
-    public IList<IList<string>> SuggestedProducts(string[] products, string searchWord)
+    /*
+     * Given an array of strings products and a string searchWord. We want to design a system that suggests at most three product names 
+     * from products after each character of searchWord is typed. Suggested products should have common prefix with the searchWord. 
+     * If there are more than three products with a common prefix return the three lexicographically minimums products.
+     * Return list of lists of the suggested products after each character of searchWord is typed. 
+     */
+    public static IList<IList<string>> SuggestedProducts(string[] products, string searchWord)
     {
-      return null;
-      //if (grid == null)
-      //{
-      //  throw new ArgumentNullException(nameof(grid), "Grid input paramter should not be null");
-      //}
-
-      ////var rows = grid.Length;
-
-      ////if no elements are fresh then return zero
-      //var gridArray=grid.SelectMany(x => x).ToArray();
-      //if (gridArray.All(x => x == (char)Element.WATER) == true)
-      //{
-      //  return 0;
-      //}
-      //if (gridArray.All(x => x == (char)Element.LAND) == true)
-      //{
-      //  return 1;
-      //}
-      //return 3;
-      //var rottenOrangesLocation = new Queue<int[]>();
-      //for (int row = 0; row < rows; row++)
-      //{
-      //  var columns=grid[row].Length;
-      //  for (int column = 0; column < columns; column++)
-      //  {
-      //    var item = grid[row][column];
-      //    if (columns == 0 || columns > 10)
-      //    {
-      //      return 0;
-      //    }
-
-      //    if (item == (int)Element.ROTTEN)
-      //    {
-      //      rottenOrangesLocation.Enqueue(new int[] { row, column });
-      //    }
-      //  }
-      //}
-
-      //int timeElapsed = 0;
-      //while (rottenOrangesLocation.Count>0)
-      //{
-      //  var rottenOrangesCount=rottenOrangesLocation.Count;
-      //  for (int counter = 0; counter < rottenOrangesCount; counter++)
-      //  {
-      //    var item = rottenOrangesLocation.Dequeue();
-      //    var row = item[0];
-      //    var column = item[1];
-      //    var columns = grid[row].Length;
-      //    //itemLeft
-      //    if (column - 1 >= 0 && grid[row][column - 1] == (int)Element.FRESH)
-      //    {
-      //      grid[row][column - 1] = (int)Element.ROTTEN;
-      //      rottenOrangesLocation.Enqueue(new int[] { row, column - 1 });
-      //    }
-      //    //itemRight
-      //    if (column + 1 < columns && grid[row][column + 1] == (int)Element.FRESH)
-      //    {
-      //      grid[row][column + 1] = (int)Element.ROTTEN;
-      //      rottenOrangesLocation.Enqueue(new int[] { row, column + 1 });
-      //    }
-      //    //itemAbove
-      //    if (row - 1 >= 0 && grid[row - 1][column] == (int)Element.FRESH)
-      //    {
-      //      grid[row - 1][column] = (int)Element.ROTTEN;
-      //      rottenOrangesLocation.Enqueue(new int[] { row - 1, column });
-      //    }
-      //    //itemBelow
-      //    if (row + 1 < rows && grid[row + 1][column] == (int)Element.FRESH)
-      //    {
-      //      grid[row + 1][column] = (int)Element.ROTTEN;
-      //      rottenOrangesLocation.Enqueue(new int[] { row + 1, column });
-      //    }
-      //  }
-      //  timeElapsed += 1;
-
-      //  if (grid.SelectMany(x => x).ToArray().All(x => new[] { (int)Element.ROTTEN, (int)Element.EMPTY }.Contains(x)) == true)
-      //  {
-      //    break;
-      //  }
-      //}
-      ////if any of the elements are still fresh then it means that they are unreachable, return -1 in this case
-      //if (grid.SelectMany(x => x).ToArray().Any(x => x==(int)Element.FRESH) == true)
-      //{
-      //  return -1;
-      //}
-      //return timeElapsed;
+      IList<IList<string>> result = new List<IList<string>>();
+      if (products == null || searchWord == null)
+      {
+        return result;
+      }
+      var orderedProducts = products.OrderBy(x => x).ToList();
+      for (var counter = 0; counter < searchWord.Length; counter++)
+      {
+        var searchString = searchWord.Substring(0,counter+1);
+        var partialList = orderedProducts.Where(x => x.StartsWith(searchString, StringComparison.InvariantCultureIgnoreCase));
+        if (partialList.Any())
+        {
+          result.Add(partialList.Take(3).ToList());
+        }
+        else
+        {
+          result.Add(new List<string> {""});
+        }
+      }
+      return result;
     }
   }
 }
