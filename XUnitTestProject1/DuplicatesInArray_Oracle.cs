b2b60652a257fa31ca817f988df43c8d9a6ad92c
ajit Goel <ajitgoel@gmail.com>
2022-03-16 20:27:44 -0500

    3/16/2022

---
+++ XUnitTestProject1/DuplicatesInArray_Oracle.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Xunit;
+using FluentAssertions;
+
+namespace XUnitTestProject1
+{
+  public class DuplicatesInArray_Oracle
+  {
+    [Fact]
+    public void Test()
+    {
+      var result1 = DoesArrayContainDuplicates(new int[] { 1, 2, 37, 56, 2 });
+      result1.Should().BeTrue();
+
+      var result2 = DoesArrayContainDuplicates(new int[] { 1, 2, 37, 56, 87 });
+      result2.Should().BeFalse();
+    }
+    public static bool DoesArrayContainDuplicates(int[] words)
+    {
+      return words.GroupBy(x => x).Any(x => x.Count()>1);
+    }
+  }
+}
