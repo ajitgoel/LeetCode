80168d62e33edeeb6203cc3848662425440e09b3
ajit Goel <ajitgoel@gmail.com>
2022-04-03 12:38:46 -0500

    4/3/2022

--- XUnitTestProject1/Arrays/GenerateArray.cs
+++ XUnitTestProject1/Arrays/GenerateArray.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Linq;
 using Newtonsoft.Json;
 
-namespace XUnitTestProject1
+namespace Oracle
 {
   public class GenerateArray
   {
@@ -17,7 +17,7 @@ namespace XUnitTestProject1
     public void Test1()
     {
       var array2 = Generate(2);
-      var array3 = Generate(3);
+      var array3 = Generate(3);//does not work for 3 by 3 array.
       var array4 = Generate(4);
       var array5 = Generate(5);
 
@@ -37,6 +37,15 @@ namespace XUnitTestProject1
       {
         for (int inner = 0; inner < arraylength; inner++)
         {
+          if ((inner - 1 > 0 && result[outer, inner] != null && result[outer, inner - 1] != null &&
+            result[outer, inner] == result[outer, inner - 1]) ||
+            (inner + 1 < arraylength && result[outer, inner] != null && result[outer, inner + 1] != null &&
+              result[outer, inner] == result[outer, inner + 1]) ||
+            (outer + 1 < arraylength && result[outer, inner] != null && result[outer + 1, inner] != null &&
+              result[outer, inner] == result[outer + 1, inner] ||
+            (outer - 1 > 0 && result[outer, inner] != null && result[outer - 1, inner] != null &&
+              result[outer, inner] == result[outer - 1, inner]))
+          )
           {
             colorelement++;
             if (colorelement >= colors.Length)
@@ -45,7 +54,14 @@ namespace XUnitTestProject1
             }
             result[outer, inner] = colors[colorelement].ToString();
           }
+          else
+          {
+            result[outer, inner] = colors[colorelement].ToString();
+            colorelement++;
+            if (colorelement >= colors.Length)
             {
+              colorelement = 0;
+            }
           }
         }
       }
