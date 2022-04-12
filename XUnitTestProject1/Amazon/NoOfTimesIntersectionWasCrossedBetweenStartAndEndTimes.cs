ad8df3ff4a774ec354bccff0a6f87b66908701b7
AjitGoel <AjitGoel@gmail.com>
2022-04-12 15:05:57 -0500

    4/12/2022

---
+++ XUnitTestProject1/Amazon/NoOfTimesIntersectionWasCrossedBetweenStartAndEndTimes.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using Xunit;
+using FluentAssertions;
+using System.Linq;
+
+namespace Amazon
+{
+  public struct Crossing
+  {
+    public long Time { get; set; }
+    public Location Location { get; set; }
+  }
+  /*https://stackoverflow.com/questions/28068123/double-or-decimal-for-latitude-longitude-values-in-c-sharp
+   * A double has up to 15 decimal digits of precision. So, lets assume three of those digits are going to be on the 
+   * left of the decimal point for lat/long values (max of 180deg). This leaves 12 digits of precision on the right. 
+   * Since a degree of lat/long is ~111km, 5 of those 12 digits would give us precision to the meter. 
+   * 3 more digits would give us precision to the millimeter. The remaining 4 digits would get us precision to 
+   * around 100 nanometers. Since double will win from the perspective of performance and memory, 
+   * I see no reason to even consider using decimal.
+   
+    float=> 32 bites=> 4 bytes
+    double=>64 bites=> 8 bytes
+    double.Epsilon=> Represents the smallest positive System.Double value that is greater than zero.
+   */
+  public struct Location
+  {
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+  }
+  public class NoOfTimesIntersectionWasCrossedBetweenStartAndEndTimes
+  {
+    public int CalculateFrequency(long startTime, long endTime, Location location, List<Crossing> crossings)
+    {
+      return crossings.Count(x => x.Time >= startTime && x.Time <= endTime &&
+        System.Math.Abs(x.Location.Longitude - location.Longitude) < double.Epsilon && 
+        System.Math.Abs(x.Location.Latitude - location.Latitude) < double.Epsilon); 
+    }    
+    [Fact]
+    public void Test2()
+    {
+      var crossings = new List<Crossing>() 
+      { 
+        new Crossing() { Location = new Location() { Latitude = 11, Longitude = 20 }, Time = 123 },
+        new Crossing() { Location = new Location() { Latitude = 21, Longitude = 30 }, Time = 123 },
+        new Crossing() { Location = new Location() { Latitude = 31, Longitude = 40 }, Time = 123 },
+        new Crossing() { Location = new Location() { Latitude = 11, Longitude = 20 }, Time = 124 },
+      };
+      CalculateFrequency(123, 124, new Location() { Latitude = 11, Longitude = 20 }, crossings).Should().Be(2);
+    }
+  }
+}
